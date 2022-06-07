7b6d436516e98c407607a2f22172528264acc517
Mas Sahal <52941780+MasSahal@users.noreply.github.com>
2022-06-07 21:37:50 +0700

    rev 2

--- praktikum/database.php
+++ praktikum/database.php
@@ -87,8 +87,8 @@ class Database
         return mysqli_query($this->koneksi, "delete from tbl_distributor where kd_distributor = '$kd_distributor'");
     }
 
-    //  Data Barang Masuk
 
+    //  Data Barang Masuk
     function data_barang_masuk()
     {
         return mysqli_query($this->koneksi, "select * from tbl_barang_masuk JOIN tbl_barang ON tbl_barang_masuk.kd_barang = tbl_barang.kd_barang JOIN tbl_distributor ON tbl_barang_masuk.kd_distributor = tbl_distributor.kd_distributor");
@@ -97,5 +97,34 @@ class Database
     function input_barang_masuk($no_ref, $kd_barang, $kd_distributor, $jumlah, $tgl_masuk, $penerima, $ket, $total)
     {
         return mysqli_query($this->koneksi, "insert into tbl_barang_masuk values ('$no_ref', '$kd_barang', '$kd_distributor', '$jumlah', '$tgl_masuk', '$penerima', '$ket', '$total')");
+    }
+
+
+    //  Data Barang keluar
+    function data_barang_keluar()
+    {
+        return mysqli_query($this->koneksi, "select * from tbl_barang_keluar JOIN tbl_barang ON tbl_barang_keluar.kd_barang = tbl_barang.kd_barang");
+    }
+
+    function input_barang_keluar($no_ref, $kd_barang, $tanggal_keluar, $jumlah, $total, $diskon, $penerima)
+    {
+        return mysqli_query($this->koneksi, "insert into tbl_barang_keluar values ('$no_ref', '$kd_barang', '$tanggal_keluar', '$jumlah', '$total', '$diskon', '$penerima')");
+    }
+
+    function delete_barang_keluar($no_ref)
+    {
+        return mysqli_query($this->koneksi, "delete from tbl_barang_keluar where no_ref = '$no_ref'");
+    }
+
+    public function tampil_update_barang_keluar($no_ref)
+    {
+        $query = mysqli_query($this->koneksi, "select * from tbl_barang_keluar JOIN tbl_barang ON tbl_barang_keluar.kd_barang = tbl_barang.kd_barang where no_ref = '$no_ref' ");
+
+        return mysqli_fetch_assoc($query);
+    }
+
+    public function update_barang_keluar($no_ref, $kd_barang, $tanggal_keluar, $jumlah, $total, $diskon, $penerima)
+    {
+        return mysqli_query($this->koneksi, "update tbl_barang_keluar set kd_barang='$kd_barang', tanggal_keluar='$tanggal_keluar', jumlah='$jumlah', total='$total', diskon='$diskon', penerima='$penerima' where no_ref='$no_ref'");
     }
 }
--- praktikum/menu.php
+++ praktikum/menu.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
@@ -17,13 +16,12 @@
                     <a class="nav-link active" aria-current="page" href="./data_distributor.php">Data Distributor</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Penjualan Barang
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
                         <li><a class="dropdown-item" href="data_barang_masuk.php">Barang Masuk</a></li>
-                        <li><a class="dropdown-item" href="#">Barang Keluar</a></li>
+                        <li><a class="dropdown-item" href="data_barang_keluar.php">Barang Keluar</a></li>
                     </ul>
                 </li>
             </ul>
